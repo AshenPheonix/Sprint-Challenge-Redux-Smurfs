cc4cfbccb64ac1613cbdfc50901652bd428733c4
Brandon Porter <brendlorend@gmail.com>
2019-06-21 09:25:48 -0700

    WIP: Actions done

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,7 +1,15 @@
+import {ALL,ONE} from '../helpers/endpoints'
+import axios from 'axios'
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+export const ADD_SMURF='ADD_SMURF'
+export const GET_SMURFS='GET_SMURFS'
+export const UPDATE_SMURF='UPDATE_SMURF'
+export const DELETE_SMURF='DELETE_SMURF'
+export const LOADING='LOADING'
+export const ERROR='ERROR'
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
@@ -13,3 +21,42 @@
    U - updateSmurf
    D - deleteSmurf
 */
+export const getSmurfs=()=>dispatch=>{
+  dispatch({type:LOADING})
+  let request=axios.get(ALL)
+  request.then(({data})=>{
+    dispatch({type:GET_SMURFS,payload:data})
+  }).catch(err=>{
+    dispatch({type:ERROR,payload:err.response.data.error})
+  })
+}
+
+export const addSmurf=e=>dispatch=>{
+  dispatch({type:LOADING})
+  let request=axios.post(ALL)
+  request.then(({data})=>{
+    dispatch({type:GET_SMURFS,payload:data})
+  }).catch(err=>{
+    dispatch({type:ERROR,payload:err.response.data.error})
+  })
+}
+
+export const updateSmurf=e=>dispatch=>{
+  dispatch({type:LOADING})
+  let request=axios.put(ONE(e.id),e)
+  request.then(({data})=>{
+    dispatch({type:GET_SMURFS,payload:data})
+  }).catch(err=>{
+    dispatch({type:ERROR,payload:err.response.data.error})
+  })
+}
+
+export const deleteSmurf=e=>dispatch=>{
+  dispatch({type:LOADING})
+  let request=axios.delete(ONE(e.id))
+  request.then(({data})=>{
+    dispatch({type:GET_SMURFS,payload:data})
+  }).catch(err=>{
+    dispatch({type:ERROR,payload:err.response.data.error})
+  })
+}
---
+++ smurfs/src/helpers/endpoints.js
@@ -0,0 +1,3 @@
+export const CORE='http://localhost:3333/'
+export const ALL=`${CORE}smurfs`
+export const ONE=e=>`${CORE}${e}`
